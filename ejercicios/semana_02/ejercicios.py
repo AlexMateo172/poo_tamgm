37486fca24e4e931d6cb99f77cf82a2e7a31d0ff
AlexMateo172 <1725110135@utectulancingo.edu.mx>
2026-05-13 14:47:55 +0000

    CREATED ejercicio ejercicios.py

---
+++ ejercicios/semana_02/ejercicios.py
@@ -0,0 +1,10 @@
+class NombreClase:
+
+    def __init__(self):
+        print("Constructor")
+
+    def metodoUno(self):
+        print("Meotodo Uno")    
+
+nombre_objeto = NombreClase()
+nombre_objeto.metodoUno()
