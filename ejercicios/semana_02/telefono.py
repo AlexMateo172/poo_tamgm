b2ba5cae37f888607e94c47575e61a7821b8e279
AlexMateo172 <1725110135@utectulancingo.edu.mx>
2026-05-19 19:34:30 +0000

    CREATED .ejercicios

---
+++ ejercicios/semana_02/telefono.py
@@ -0,0 +1,11 @@
+class Telefono:
+     
+     def __init__(self, marca, modelo, capacidad_de_bateria, sistema_operativo, almacenamiento, color, resolucion_de_pantalla, memoria_ram, procesador, refrigeracion):
+          
+        
+        
+        self.marca = marca
+        self.modelo = modelo
+        self.capacidad_de_bateria = capacidad_de_bateria
+        self
+          
